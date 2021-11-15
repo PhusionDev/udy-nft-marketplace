f1d3fafb9dcdaa8fd88b95ee5e5f89db9ab58b9c
Jared Hodgkins <phusiondev@gmail.com>
2021-11-15 14:25:29 -0500

    Up to 118. Transfer Event for Minting NFTs

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ERC721 {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    // mapping from token id to the owner
+    mapping(uint256 => address) private _tokenOwner;
+
+    // mapping from owner to number of owned tokens
+    mapping(address => uint256) private _ownedTokensCount;
+
+    function _mint(address to, uint256 tokenId) internal {
+        // requires that the address isn't zero
+        require(to != address(0), 'ERC721: minting to the zero address');
+        // requires that the token does not already exist
+        require(!_exists(tokenId), 'ERC721: token already minted');
+        // we are adding a new address with a token id for minting
+        _tokenOwner[tokenId] = to;
+        // keeping track of each address that is minting and adding 1 to count
+        _ownedTokensCount[to]++;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    function _exists(uint256 tokenId) internal view returns(bool) {
+        // setting the address of nft owner to check the mapping
+        // of the address from tokenOwner at the tokenId
+        address owner = _tokenOwner[tokenId];
+        // returns truthiness the address is not zero
+        return owner != address(0);
+    }
+}
