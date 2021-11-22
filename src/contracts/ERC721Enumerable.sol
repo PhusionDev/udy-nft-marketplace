af7433ca2313cb275aa472fd57d4d2c35ae909b9
Jared Hodgkins <phusiondev@gmail.com>
2021-11-22 14:22:24 -0500

    160. Integrating the ERCEnumerable interface

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import './ERC721.sol';
+import './interfaces/IERC721Enumerable.sol';
 
-contract ERC721Enumerable is ERC721 {
+contract ERC721Enumerable is IERC721Enumerable, ERC721 {
     uint256[] private _allTokens;
 
     // mapping from tokenId to position in _allTokens array
@@ -16,17 +17,17 @@ contract ERC721Enumerable is ERC721 {
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
     // return the total supply of the _allTokens array
-    function totalSupply() public view returns (uint256) {
+    function totalSupply() public override view returns (uint256) {
         return _allTokens.length;
     }
 
-    function tokenByIndex(uint256 index) public view returns (uint256) {
+    function tokenByIndex(uint256 index) public override view returns (uint256) {
         // make sure that the index is not out of bounds of the total supply
         require(index < totalSupply(), 'global index is out of bounds');
         return _allTokens[index];
     }
 
-    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
+    function tokenOfOwnerByIndex(address owner, uint256 index) public override view returns (uint256) {
         require(index < this.balanceOf(owner),'owner index is out of bounds');
         return _ownedTokens[owner][index];
     }
