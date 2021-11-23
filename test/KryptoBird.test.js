a1433571b84f83ddb55ae825f0d5a3d0c2655bff
Jared Hodgkins <phusiondev@gmail.com>
2021-11-23 12:24:34 -0500

    167. Javascript NFT test container basics

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -1,4 +1,5 @@
 const assert = require('chai');
+const _deploy_contracts = require('../migrations/2_deploy_contracts');
 
 const KryptoBird = artifacts.require('./KryptoBird');
 
@@ -6,3 +7,15 @@ const KryptoBird = artifacts.require('./KryptoBird');
 require('chai')
   .use(require('chai-as-promised'))
   .should();
+
+contract('KryptoBird', (accounts) => {
+  let contract;
+
+  // testing container - describe
+  describe('deployment', async () => {
+    // test samples with writing it
+    it('deploys successfully', async () => {
+      contract = await KryptoBird.deployed();
+    });
+  });
+});
