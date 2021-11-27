a31a2dca6f3fb8167015809e6de3b27b20f253ff
Jared Hodgkins <phusiondev@gmail.com>
2021-11-26 21:40:20 -0500

    211. Connecting metamask and ethereum provider to the front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import KryptoBird from "../abis/KryptoBird.json";
 
 class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3();
+  }
+
+  // first up is to detect ethereum provider
+  async loadWeb3() {
+    const provider = await detectEthereumProvider();
+
+    // modern browsers
+    // if there's a provider then lets
+    // log that it's working and access the window from the doc
+    // to set web3 to the provider
+    if (provider) {
+      console.log("ethereum wallet is connected");
+      window.ethereum = new Web3(provider);
+    } else {
+      // no ethereum provider
+      console.log("no ethereum wallet detected");
+    }
+  }
   render() {
     return (
       <div>
