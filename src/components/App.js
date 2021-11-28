af86ec4797770b8e53bf1bb4d7942871942f106a
Jared Hodgkins <phusiondev@gmail.com>
2021-11-27 20:48:00 -0500

    217. Load the NFT marketplace to the front end (Web3)

--- src/components/App.js
+++ src/components/App.js
@@ -4,57 +4,68 @@ import detectEthereumProvider from "@metamask/detect-provider";
 import KryptoBird from "../abis/KryptoBird.json";
 
 class App extends Component {
-  async componentDidMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
-
-  // first up is to detect ethereum provider
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider);
-    } else {
-      console.log(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
+    async componentDidMount() {
+        await this.loadWeb3();
+        await this.loadBlockchainData();
     }
-  }
 
-  async loadBlockchainData() {
-    const web3 = window.web3;
-    const acc = await web3.eth.getAccounts();
-    this.setState({ account: acc[0] });
-  }
+    // first up is to detect ethereum provider
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable();
+        } else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider);
+        } else {
+            console.log(
+                "Non-Ethereum browser detected. You should consider trying MetaMask!"
+            );
+        }
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: "",
-    };
-  }
+    async loadBlockchainData() {
+        const web3 = window.web3;
+        const acc = await web3.eth.getAccounts();
+        this.setState({ account: acc[0] });
 
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <div
-            className="navbar-brand col-sm-3 col-md-3 mr-0"
-            style={{ color: "white" }}
-          >
-            KryptoBirdz NFTs (Non Fungible Tokens)
-          </div>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white">{this.state.account}</small>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+        const networkId = await web3.eth.net.getId();
+        const networkData = KryptoBird.networks[networkId];
+        if (networkData) {
+            const abi = KryptoBird.abi;
+            const address = networkData.address;
+            const contract = new web3.eth.Contract(abi, address);
+            console.log(contract);
+        }
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: "",
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+                    <div
+                        className="navbar-brand col-sm-3 col-md-3 mr-0"
+                        style={{ color: "white" }}
+                    >
+                        KryptoBirdz NFTs (Non Fungible Tokens)
+                    </div>
+                    <ul className="navbar-nav px-3">
+                        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+                            <small className="text-white">
+                                {this.state.account}
+                            </small>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default App;
