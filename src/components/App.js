b8ede6957586405e106990653edd8928c65a96fa
Jared Hodgkins <phusiondev@gmail.com>
2021-11-30 12:33:14 -0500

    225. Final minting functionality for the front end

--- src/components/App.js
+++ src/components/App.js
@@ -77,6 +77,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        {console.log(this.state.kryptoBirdz)}
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <div
             className="navbar-brand col-sm-3 col-md-3 mr-0"
