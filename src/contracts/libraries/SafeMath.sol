15df09fb66bf7f6865cf5dd8e3a76cd0e6d06eb0
Jared Hodgkins <phusiondev@gmail.com>
2021-12-01 14:38:09 -0500

    237. Solution - complete the solidity SafeMath

--- src/contracts/libraries/SafeMath.sol
+++ src/contracts/libraries/SafeMath.sol
@@ -17,4 +17,29 @@ library SafeMath {
     require(b <= a, 'SafeMath subtraction overflow');
     return r;
   }
+
+  // function multiply r = a * b
+  function mul(uint256 a, uint256 b) internal pure returns(uint256) {
+    // gas optimization
+    if (a == 0 || b == 0) {
+      return 0;
+    }
+    uint256 r = a * b;
+    require(r / a == b, 'SafeMath multiplication overflow');
+    return r;
+  }
+
+  // function divide r = a / b
+  function div(uint256 a, uint256 b) internal pure returns(uint256) {
+    require(b > 0, 'SafeMath division overflow');
+    uint256 r = a / b;
+    return r;
+  }
+
+  // function modulo r = a % b
+  // gas spending remains untouched
+  function mod(uint256 a, uint256 b) internal pure returns(uint256) {
+    require(b != 0, 'SafeMath modulo by zero');
+    return a % b;
+  }
 }
